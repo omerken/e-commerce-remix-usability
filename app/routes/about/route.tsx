c7cdcff18454ded9b89cbc2f4035350bb6241503
Yurii Venher <yuriiv@wix.com>
2024-07-29 16:07:49 +0300

    Remove author meta tag

--- app/routes/about/route.tsx
+++ app/routes/about/route.tsx
@@ -41,10 +41,6 @@ export const meta: MetaFunction<typeof loader> = ({ data }) => {
             content: description,
         },
         {
-            name: 'author',
-            content: 'Codux',
-        },
-        {
             tagName: 'link',
             rel: 'canonical',
             href: data?.canonicalUrl,
