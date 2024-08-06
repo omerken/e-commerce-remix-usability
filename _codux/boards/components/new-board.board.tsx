7cd538c4ec62cc465eb896749a51edf39a6b5f6f
ShireeWill <shiree@wix.com>
2024-08-06 16:52:47 +0300

    small-fixes

--- _codux/boards/components/new-board.board.tsx
+++ _codux/boards/components/new-board.board.tsx
@@ -6,6 +6,7 @@ export default createBoard({
     Board: () => (
         <div className={styles.layout}>
             <h4 className={styles.h42}>
+                <h1 className={styles.h12}>65px</h1>
                 Looks like there&apos;s nothing here.
                 <p className={styles.p2}>
                     The link you followed may be broken, or the page might have moved.
@@ -19,5 +20,6 @@ export default createBoard({
     isSnippet: true,
     environmentProps: {
         windowWidth: 832,
+        windowHeight: 647,
     },
 });
