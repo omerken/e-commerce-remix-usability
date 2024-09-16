219977c780719877c3ab29e5d363a0380922b5d3
Daniel Cohen <danielcoh@wix.com>
2024-09-16 19:00:20 +0300

    Refactor typography and color styles

--- _codux/boards/intro/intro.board.tsx
+++ _codux/boards/intro/intro.board.tsx
@@ -10,9 +10,9 @@ export default createBoard({
                 HOW TO USE <br /> E-COMMERCE STARTER?
             </p>
             <p className={styles.myParagraph}>
-                Welcome to Codux e-commerce starter. <br /> This template was built to help you
-                create your own store easily. Use Codux to design your website the way you want, and
-                Wix Headless e-commerce services to manage your store.
+                Welcome to Codux e-commerce starter. <br /> This template was built to help you create your own store
+                easily. Use Codux to design your website the way you want, and Wix Headless e-commerce services to
+                manage your store.
                 <a href={'https://help.codux.com/kb/en/article/kb37137'}>
                     <button className={commonStyles.introButton}>Learn more</button>
                 </a>
