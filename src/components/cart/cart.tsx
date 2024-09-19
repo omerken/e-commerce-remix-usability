f7d36f9bcb4bf286c54d07ed66a407b06308f9cc
olehrakwix <93204312+olehrakwix@users.noreply.github.com>
2024-09-19 11:57:42 +0300

    fix: remove redundant wrapper (#44)

--- src/components/cart/cart.tsx
+++ src/components/cart/cart.tsx
@@ -7,11 +7,7 @@ import { CartItem } from './cart-item/cart-item';
 import { useCartOpen } from './cart-open-context';
 import styles from './cart.module.scss';
 
-export interface CartProps {
-    className?: string;
-}
-
-export const Cart = ({ className }: CartProps) => {
+export const Cart = () => {
     const { isOpen, setIsOpen } = useCartOpen();
     const { data: cart } = useCart();
     const { data: cartTotals } = useCartTotals();
@@ -30,32 +26,27 @@ export const Cart = ({ className }: CartProps) => {
     }
 
     return (
-        <div className={className}>
-            <Drawer title="Cart" onClose={() => setIsOpen(false)} isOpen={isOpen}>
-                {isEmpty ? (
-                    <div className={styles.emptyCart}>Cart is empty</div>
-                ) : (
-                    <div className={styles.cart}>
-                        <div className={styles.items}>
-                            {cart?.lineItems?.map((item) => (
-                                <CartItem key={item._id} cartItem={item} />
-                            ))}
-                        </div>
-                        <div className={styles.subtotalCheckout}>
-                            <label className={styles.subtotalLabel}>
-                                <span>Subtotal:</span>
-                                {cartTotals?.priceSummary?.subtotal?.formattedConvertedAmount}
-                            </label>
-                            <button
-                                className={Classnames(commonStyles.primaryButton, styles.checkout)}
-                                onClick={checkout}
-                            >
-                                Checkout
-                            </button>
-                        </div>
+        <Drawer title="Cart" onClose={() => setIsOpen(false)} isOpen={isOpen}>
+            {isEmpty ? (
+                <div className={styles.emptyCart}>Cart is empty</div>
+            ) : (
+                <div className={styles.cart}>
+                    <div className={styles.items}>
+                        {cart?.lineItems?.map((item) => (
+                            <CartItem key={item._id} cartItem={item} />
+                        ))}
                     </div>
-                )}
-            </Drawer>
-        </div>
+                    <div className={styles.subtotalCheckout}>
+                        <label className={styles.subtotalLabel}>
+                            <span>Subtotal:</span>
+                            {cartTotals?.priceSummary?.subtotal?.formattedConvertedAmount}
+                        </label>
+                        <button className={Classnames(commonStyles.primaryButton, styles.checkout)} onClick={checkout}>
+                            Checkout
+                        </button>
+                    </div>
+                </div>
+            )}
+        </Drawer>
     );
 };
