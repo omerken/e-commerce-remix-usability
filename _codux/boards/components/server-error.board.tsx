ff5335cf43e32d5ed00cc58bfd58751df3a635f5
ShireeWill <shiree@wix.com>
2024-09-23 12:01:39 +0300

    style: utilized variables to all error pages

--- _codux/boards/components/server-error.board.tsx
+++ _codux/boards/components/server-error.board.tsx
@@ -10,15 +10,10 @@ export default createBoard({
         <div className={styles.layout}>
             <ServererrorSvg className={styles.servererrorSvg} />
             <div className={styles.div1}>
-                <h1 className={styles.h4}>Something went wrong.</h1>
-                <p className={styles.p}>Try reloading the page.</p>
+                <h1 className={styles.h4}>Something went wrong</h1>
+                <p className={styles.p}>Try reloading the page</p>
             </div>
-            <button
-                className={Classnames(
-                    styles.secondaryButtonVar1,
-                    commonStylesStyles.secondaryButton
-                )}
-            >
+            <button className={Classnames(styles.secondaryButtonVar1, commonStylesStyles.secondaryButton)}>
                 Reload Page
             </button>
         </div>
